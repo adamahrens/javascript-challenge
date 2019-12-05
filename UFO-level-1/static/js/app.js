7325c19acfeb2ea458b1bbba983ba764fd6a2330
Adam Ahrens <adam.aahrens@gmail.com>
2019-12-04 21:59:33 -0600

    populating the table

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,27 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+var htmlTable = document.getElementById('ufo-table');
+
+tableData.forEach(function(item) {
+  console.log(item);
+
+  // Create row and all the td elements
+  var row = htmlTable.insertRow(-1);
+  var cell1 = row.insertCell(0);
+  var cell2 = row.insertCell(1);
+  var cell3 = row.insertCell(2);
+  var cell4 = row.insertCell(3);
+  var cell5 = row.insertCell(4);
+  var cell6 = row.insertCell(5);
+  var cell7 = row.insertCell(6);
+
+  // Set the innerHTML
+  cell1.innerHTML = item.datetime;
+  cell2.innerHTML = item.city;
+  cell3.innerHTML = item.state;
+  cell4.innerHTML = item.country;
+  cell5.innerHTML = item.shape;
+  cell6.innerHTML = item.durationMinutes;
+  cell7.innerHTML = item.comments;
+});
